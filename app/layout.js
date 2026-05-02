27a10657aba9f1e1026a6c7cd6aff2d2779af776
Adam Charest <matthewwweb@gmail.com>
2026-05-02 01:24:32 -0400

    Add Google AdSense script and ads txt

--- app/layout.js
+++ app/layout.js
@@ -97,6 +97,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3247864013895587"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+
       <body>
         <SiteHeader />
         {children}
